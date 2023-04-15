31aa34b3a11b8328321efafbde761d7beeb0c9c3
Jan Termion <jan.termion@gmail.com>
2023-04-15 21:29:12 +0200

    zadanie wykonane

--- src/main/java/pl/javastart/voting/Voting.java
+++ src/main/java/pl/javastart/voting/Voting.java
@@ -6,10 +6,12 @@ import java.util.Scanner;
 
 public class Voting {
 
+    private static final String FOR = "z";
+    private static final String AGAINST = "p";
+
     public static void main(String[] args) {
         List<String> voters = new ArrayList<>();
 
-        // możesz dowolnie dodawać / usuwać dane testowe
         voters.add("Jan Kowalski");
         voters.add("Zigniew Siobro");
         voters.add("Zbyszek Stonoga");
@@ -26,9 +28,37 @@ public class Voting {
      * do wczytywania danych od użytkownika użyj scannera z parametru
      * Metoda powinna pobrać głos dla każdego przekazanego głosującego i zapisać wyniki głosowania do VotingResult
      */
-    VotingResult executeVoting(List<String> voters, Scanner scanner) {
 
-        return null; // to możesz (a nawet powinieneś/powinnaś) zmienić :)
+    VotingResult executeVoting(List<String> voters, Scanner scanner) {
+        VotingResult votingResult = new VotingResult();
+        for (String voter : voters) {
+            Boolean voting = readVote(scanner, voter);
+            votingResult.addVoter(voter, voting);
+        }
+        return votingResult;
     }
 
+    private Boolean readVote(Scanner scanner, String voter) {
+
+        System.out.println("Jak głosuje " + voter + "? (z - za, p - przeciw, w - wstrzymanie się)");
+        Boolean result;
+        String validVote = "";
+        String vote;
+
+        while (validVote.equals("")) {
+            vote = scanner.nextLine();
+            if ((vote.equals("z")) || (vote.equals("p")) || (vote.equals("w"))) {
+                validVote = vote;
+            } else {
+                System.out.println("nie ma takiej opcji, proszę spóbować jeszcze raz");
+            }
+        }
+        switch (validVote) {
+            case FOR -> result = true;
+            case AGAINST -> result = false;
+            default -> result = null;
+        }
+        return result;
+    }
 }
+
